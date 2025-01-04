9155bb8939df66848cd79c1ff5f4318e8a7b1f37
jinhwan Bea <paewhal20@gmail.com>
2025-01-04 15:28:35 +0900

    백준 10950번 A+B - 3

---
+++ src/bronze5/N10950.java
@@ -0,0 +1,28 @@
+package bronze5;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class N10950 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write(a + b + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
