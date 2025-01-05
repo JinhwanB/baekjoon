98d8881a913471fb39a2d2659372caba3c7b23bc
jinhwan Bea <paewhal20@gmail.com>
2025-01-05 14:49:16 +0900

    백준 25304번 영수증

---
+++ src/bronze4/N25304.java
@@ -0,0 +1,37 @@
+package bronze4;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class N25304 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int x = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sum += (a * b);
+        }
+
+        if (x == sum) {
+            bw.write("Yes");
+        } else {
+            bw.write("No");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
