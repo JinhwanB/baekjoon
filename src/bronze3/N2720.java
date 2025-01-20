8512be33d0d885774e8d09e363e026ee4bca8e81
jinhwan Bea <paewhal20@gmail.com>
2025-01-20 23:16:56 +0900

    백준 2720번 - 세탁소 사장 동혁

---
+++ src/bronze3/N2720.java
@@ -0,0 +1,38 @@
+package bronze3;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class N2720 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            int c = Integer.parseInt(br.readLine());
+
+            int quarter = c / 25;
+            c %= 25;
+
+            int dime = c / 10;
+            c %= 10;
+
+            int nickel = c / 5;
+            c %= 5;
+
+            int penny = c;
+
+            bw.write(quarter + " " + dime + " " + nickel + " " + penny + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
