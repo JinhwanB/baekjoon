276d3d38b10b74554806bf52af1fc3ece6f7b29b
jinhwan Bea <paewhal20@gmail.com>
2025-01-28 12:04:22 +0900

    백준 1085번 - 직사각형에서 탈출

---
+++ src/bronze3/N1085.java
@@ -0,0 +1,30 @@
+package bronze3;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class N1085 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+        br.close();
+
+        int result = Math.min(Math.min(x, y), Math.min(w - x, h - y));
+
+        bw.write(String.valueOf(result));
+        bw.flush();
+        bw.close();
+    }
+
+}
