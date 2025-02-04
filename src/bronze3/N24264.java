c26708c734141720f96a98ef6c98764ee720db73
jinhwanB <paewhal20@gmail.com>
2025-02-04 16:29:43 +0900

    백준 24264번 - 알고리즘 수업 - 알고리즘의 수행 시간 3

---
+++ src/bronze3/N24264.java
@@ -0,0 +1,22 @@
+package bronze3;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class N24264 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        br.close();
+
+        bw.write((long) Math.pow(n, 2) + "\n2");
+        bw.flush();
+        bw.close();
+    }
+
+}
