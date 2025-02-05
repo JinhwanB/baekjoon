a8c536d1d22ae25453f895a91570c30841115a31
jinhwan Bea <paewhal20@gmail.com>
2025-02-05 13:41:03 +0900

    백준 24266번 - 알고리즘 수업 - 알고리즘의 수행 시간 5

---
+++ src/bronze3/N24266.java
@@ -0,0 +1,23 @@
+package bronze3;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class N24266 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        long n = Long.parseLong(br.readLine());
+        br.close();
+
+        long result = n * n * n;
+        bw.write(result + "\n3");
+        bw.flush();
+        bw.close();
+    }
+
+}
