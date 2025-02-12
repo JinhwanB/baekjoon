14d333bd4c95eb05984d6f6d5a1ddbd3b79f3a61
jinhwanB <paewhal20@gmail.com>
2025-02-12 12:31:06 +0900

    백준 11650번 - 좌표 정렬하기

---
+++ src/silver5/N11650.java
@@ -0,0 +1,46 @@
+package silver5;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class N11650 {
+
+    static BufferedReader br;
+    static BufferedWriter bw;
+    static int n;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> {
+            if (o1[0] == o2[0]) {
+                return o1[1] - o2[1];
+            }
+
+            return o1[0] - o2[0];
+        });
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            pq.add(new int[]{x, y});
+        }
+        br.close();
+
+        while (!pq.isEmpty()) {
+            int[] poll = pq.poll();
+            bw.write(poll[0] + " " + poll[1] + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+
+}
