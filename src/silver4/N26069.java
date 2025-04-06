5057e759d847738f875b79d4d61f6affd9249a17
jinhwanB <paewhal20@gmail.com>
2025-04-07 01:55:16 +0900

    백준 26069번 - 붙임성 좋은 총총이

---
+++ src/silver4/N26069.java
@@ -0,0 +1,36 @@
+package silver4;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class N26069 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Set<String> set = new HashSet<>();
+        set.add("ChongChong");
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String a = st.nextToken();
+            String b = st.nextToken();
+            if (set.contains(a) || set.contains(b)) {
+                set.add(a);
+                set.add(b);
+            }
+        }
+
+        br.close();
+        bw.write(set.size() + "");
+        bw.flush();
+        bw.close();
+    }
+
+}
