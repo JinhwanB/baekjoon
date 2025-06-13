061c972fd60ca5eb8a42816072bd539ee12ec484
jinhwanB <paewhal20@gmail.com>
2025-06-13 10:12:23 +0900

    백준 11660번 - 구간 합 구하기 5

---
+++ src/silver1/N11660.java
@@ -0,0 +1,58 @@
+package silver1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class N11660 {
+
+    static BufferedWriter bw;
+    static int n, m;
+    static int[][] arr, sum;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n + 1][n + 1];
+        sum = new int[n + 1][n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            for (int j = 1; j <= n; j++) {
+                int curVal = Integer.parseInt(st.nextToken());
+                arr[i][j] = curVal;
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + arr[i][j];
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            int result = sum[x2][y2] - sum[x1 - 1][y2] - sum[x2][y1 - 1] + sum[x1 - 1][y1 - 1];
+
+            bw.write(result + "\n");
+        }
+
+        br.close();
+
+        bw.flush();
+        bw.close();
+    }
+
+}
